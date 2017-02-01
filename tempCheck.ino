3f73c9a91ebec90e842484879bc7521772c05313
EmilyRae995 <emilyrae995@gmail.com>
2017-01-31 19:13:26 -0500

    Add files via upload
    code to calibrate thermistor

---
+++ tempCheck.ino
@@ -0,0 +1,21 @@
+int V1 = 1;
+float Rtemp;
+float Vtemp;
+//int V2 = A2;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(V1, INPUT);
+//pinMode(V2, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ Vtemp=analogRead(V1)*0.00290;
+ 
+ Rtemp = ((3.0-Vtemp)*30.0)/Vtemp;
+ Serial.println(Rtemp,10);
+// Serial.println(Vtemp);
+ delay(100);
+}
